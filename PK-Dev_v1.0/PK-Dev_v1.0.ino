4f9cf50bb891e4ab285895c301050bb0941d139d
heylookimuphere <toshi.kuwa@gmail.com>
2014-12-06 20:19:01 -0800

    File cleanup 3

--- PK-Dev_v1.0/PK-Dev_v1.0.ino
+++ PK-Dev_v1.0/PK-Dev_v1.0.ino
@@ -1,3 +1,5 @@
+//
+
 
 #include <Wire.h>
 #include <SeeedOLED.h>
@@ -31,48 +33,51 @@ void loop()
   float temper = TH02.ReadTemperature(); 
   float humidity = TH02.ReadHumidity();
   
-  Serial.print("Temp: ");  
-  Serial.print(temper);
-  Serial.print(" C ");
-  Serial.print("Humi: ");
-  Serial.print(humidity);
-  Serial.print(" % ");
-  
-  SeeedOled.setTextXY(0,0);          //Set the cursor to Xth Page, Yth Column 
-  SeeedOled.putString("Temp: "); 
-  SeeedOled.putFloat(temper); //Print the String
-  SeeedOled.putString(" C"); 
-  SeeedOled.setTextXY(1,0);          //Set the cursor to Xth Page, Yth Column
-  SeeedOled.putString("Humi: ");  
-  SeeedOled.putFloat(humidity); //Print the String
-  SeeedOled.putString(" %");
-  
-  duration = pulseIn(pin, LOW);
-  lowpulseoccupancy = lowpulseoccupancy+duration;
-
+    duration = pulseIn(pin, LOW);
+    lowpulseoccupancy = lowpulseoccupancy+duration;
 
     ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
     concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    
+    Serial.print("Temp: ");  
+        Serial.print(temper);
+        Serial.print(" C ");
+    Serial.print("Humi: ");
+        Serial.print(humidity);
+        Serial.print(" % ");
     Serial.print("LPO: ");
-    Serial.print(lowpulseoccupancy);
+        Serial.print(lowpulseoccupancy);
     Serial.print("Ratio: ");
-    Serial.print(ratio);
+        Serial.print(ratio);
     Serial.print("Conc: ");
-    Serial.print(concentration);
+        Serial.print(concentration);
     Serial.println();
-    
-    SeeedOled.setTextXY(2,0);          //Set the cursor to Xth Page, Yth Column
-        SeeedOled.putString("LPO: ");  
-        SeeedOled.putFloat(lowpulseoccupancy); //Print the String
+  
+      SeeedOled.setTextXY(0,0);          //Set the cursor to Xth Page, Yth Column 
+          SeeedOled.putString("Temp: "); 
+          SeeedOled.putFloat(temper); //Print the String
+          SeeedOled.putString(" C"); 
+      
+      SeeedOled.setTextXY(1,0);          //Set the cursor to Xth Page, Yth Column
+          SeeedOled.putString("Humi: ");  
+          SeeedOled.putFloat(humidity); //Print the String
+          SeeedOled.putString(" %");
+      
+      SeeedOled.setTextXY(2,0);          //Set the cursor to Xth Page, Yth Column
+         SeeedOled.putString("LPO: ");  
+         SeeedOled.putFloat(lowpulseoccupancy); //Print the String
+         SeeedOled.putString("           ");  
 
-    SeeedOled.setTextXY(3,0);          //Set the cursor to Xth Page, Yth Column
-        SeeedOled.putString("Ratio: ");  
-        SeeedOled.putFloat(ratio); //Print the String
+      SeeedOled.setTextXY(3,0);          //Set the cursor to Xth Page, Yth Column
+         SeeedOled.putString("Ratio: ");  
+         SeeedOled.putFloat(ratio); //Print the String
+         SeeedOled.putString("           ");  
     
-    SeeedOled.setTextXY(4,0);          //Set the cursor to Xth Page, Yth Column
-        SeeedOled.putString("Conc: ");  
-        SeeedOled.putFloat(concentration); //Print the String
-    
+      SeeedOled.setTextXY(4,0);          //Set the cursor to Xth Page, Yth Column
+         SeeedOled.putString("Conc: ");  
+         SeeedOled.putFloat(concentration); //Print the String
+         SeeedOled.putString("           ");  
+  
     lowpulseoccupancy = 0;
     
   delay(1000);
