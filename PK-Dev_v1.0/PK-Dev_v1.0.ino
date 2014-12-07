f772676c31b08869760e641356a17c62059ed0ae
Taka <taka.kuwayama@gmail.com>
2014-12-06 19:24:03 -0800

    Dust sensor
    Capabilities for Dust sensor added, upgraded Serial output

--- PK-Dev_v1.0/PK-Dev_v1.0.ino
+++ PK-Dev_v1.0/PK-Dev_v1.0.ino
@@ -4,6 +4,15 @@
 #include <TH02_dev.h>
 #include "Arduino.h"
 
+
+int pin = 2;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 1000;//sampe 30s ;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
 void setup()
 {
   Wire.begin();
@@ -14,7 +23,11 @@ void setup()
   
   Serial.begin(9600);        // start serial for output
   SeeedOled.clearDisplay(); // Completely clear OLED
-//another hello
+  
+  //Dust Sensor
+  pinMode(2,INPUT);
+  starttime = millis();//get the current time;
+
 }
 
 void loop()
@@ -26,14 +39,16 @@ void loop()
   SeeedOled.putString("IAMGROOT");
   
   float temper = TH02.ReadTemperature(); 
-  Serial.println("Temperature: ");   
-  Serial.print(temper);
-  Serial.println("C\r\n");
-   
   float humidity = TH02.ReadHumidity();
-  Serial.println("Humidity: ");
+  
+  Serial.print("Temp: ");  
+  Serial.print(temper);
+  Serial.print(" C");
+  Serial.println();
+  Serial.print("Humi: ");
   Serial.print(humidity);
-  Serial.println("%\r\n");
+  Serial.print(" %");
+   Serial.println(); 
   
   SeeedOled.setTextXY(1,0);          //Set the cursor to Xth Page, Yth Column 
   SeeedOled.putString("Temp: "); 
@@ -44,6 +59,42 @@ void loop()
   SeeedOled.putFloat(humidity); //Print the String
   SeeedOled.putString(" %");
   
+  duration = pulseIn(pin, LOW);
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+
+  if ((millis()-starttime) > sampletime_ms)//if the sampel time == 30s
+  {
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    Serial.print("LPO: ");
+    Serial.print(lowpulseoccupancy);
+    Serial.println();
+    Serial.print("Ratio: ");
+    Serial.print(ratio);
+    Serial.println();
+    Serial.print("Conc: ");
+    Serial.print(concentration);
+    Serial.println();
+    
+    SeeedOled.setTextXY(3,0);          //Set the cursor to Xth Page, Yth Column
+    SeeedOled.putString("LPO: ");  
+    SeeedOled.putFloat(lowpulseoccupancy); //Print the String
+
+    SeeedOled.setTextXY(4,0);          //Set the cursor to Xth Page, Yth Column
+    SeeedOled.putString("Ratio: ");  
+    SeeedOled.putFloat(ratio); //Print the String
+    
+    SeeedOled.setTextXY(5,0);          //Set the cursor to Xth Page, Yth Column
+    SeeedOled.putString("Conc: ");  
+    SeeedOled.putFloat(concentration); //Print the String
+    
+    lowpulseoccupancy = 0;
+    starttime = millis();
+    
+
+    
+  }
+  Serial.println("************");
   delay(1000);
 }
 
